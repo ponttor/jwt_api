f5ef893eb268c27d5d3996d3b418c7eaf068131f
Alexander Illarionov <alexander.illarionov@dualbootpartners.com>
2024-11-04 08:19:09 +0100

    refactor error handler

--- app/controllers/concerns/error_handler.rb
+++ app/controllers/concerns/error_handler.rb
@@ -5,18 +5,12 @@ module ErrorHandler
 
   def handle_error(exception, action:)
     case exception
-    when TokenService::InvalidTokenError
-      Rails.logger.warn("#{action} failed: #{exception.message}")
-      render json: { error: exception.message }, status: :unprocessable_entity
     when ActionController::ParameterMissing
       Rails.logger.warn("#{action} failed: #{exception.message}")
       render json: { error: exception.message }, status: :bad_request
-    when JWT::EncodeError, JWT::DecodeError, JWT::ExpiredSignature, TypeError, TokenService::TokenPayloadError
+    when JWT::EncodeError, JWT::DecodeError, JWT::ExpiredSignature, TypeError, TokenService::TokenPayloadError, TokenService::InvalidTokenError, MiniMagick::Error
       Rails.logger.error("#{action} unprocessable entity: #{exception.message}")
       render json: { error: exception.message }, status: :unprocessable_entity
-    when MiniMagick::Error
-      Rails.logger.error("MiniMagick error during #{action}: #{exception.message}")
-      render json: { error: 'Failed to generate QR code image' }, status: :unprocessable_entity
     else
       Rails.logger.fatal("#{action} encountered an unexpected error: #{exception.message}")
       render json: { error: "#{action} error: #{exception.message}" }, status: :internal_server_error
